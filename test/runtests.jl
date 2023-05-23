76458b9d70ec42d7670bad84adb59365f7f14a47
Rhahi <git@rhahi.com>
2023-05-23 19:25:11 +0200

    test: add test for console

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using RemoteLogging
+using ProgressLogging
 using Test
+using Sockets
+using UUIDs
+using Logging
 
-@testset "RemoteLogging.jl" begin
-    # Write your tests here.
+include("debug_listener.jl")
+
+const HOST = IPv4(0)
+const MPORT = 50010
+const PPORT = 50011
+
+messagedata = listen_message(HOST, MPORT)
+progressdata = listen_progress(HOST, PPORT)
+
+try
+    include("test_console.jl")
+    include("test_progress.jl")
+    include("test_combined.jl")
+finally
+    close(messagedata.server)
+    close(progressdata.server)
 end

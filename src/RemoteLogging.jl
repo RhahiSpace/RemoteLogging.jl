56185ab847d1a1bdb6f248075aa0cdef65f7ab28
Rhahi <git@rhahi.com>
2023-05-23 16:38:14 +0200

    fix: add exports and update includes

--- src/RemoteLogging.jl
+++ src/RemoteLogging.jl
@@ -1,5 +1,6 @@
 module RemoteLogging
 
+using Dates
 using Logging
 using LoggingExtras
 using ProgressLogging
@@ -11,9 +12,11 @@ import Base.CoreLogging:
     AbstractLogger, SimpleLogger,
     handle_message, shouldlog, min_enabled_level, catch_exceptions
 
-export RemoteLogger
+export RemoteLogger, DiskLogger, ConsoleRemoteLogger, ProgressRemoteLogger, DataLogger
+export activate_listener
 
 include("logger.jl")
 include("listener.jl")
+include("boilerplates.jl")
 
 end
